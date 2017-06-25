64446d20cdffbb98c56c650e1395c9fb05df5e14
10606 <gav_sbor@mail.ru>
2017-06-25 20:17:05 +0300

    list

---
+++ zachet_2/list.cpp
@@ -0,0 +1,367 @@
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+
+template <typename T>
+struct list;
+template <typename T>
+struct list_base
+{
+protected:
+    list_base * prev, * next;
+    void set_link(list_base * pos, list_base * root);
+    list_base (list_base <T> const & a, list_base const * end_1);
+    list_base * split_l(list_base * a)
+    {
+        list_base * ans = a->prev;
+        a->prev = 0;
+        return ans;
+    }
+    list_base * split_r(list_base * a)
+    {
+        a->prev->next = 0;
+        return a;
+    }
+    list_base * split(list_base * a)
+    {
+        a->prev->next = a->next;
+        a->next->prev = a->prev;
+        list_base * ans = a->next;
+        a->prev = 0;
+        a->next = 0;
+        return ans;
+    }
+    void merge(list_base * a, list_base * b)
+    {
+        a->next = b;
+        b->prev = a;
+    }
+public:
+    list_base ();
+    list_base (T const &);
+    ~list_base ();
+    list_base (list_base const & a);
+    void swap (list_base b);
+    bool empty ();
+    void clear();
+    void push_back(T const & a);
+    void push_front(T const & a);
+    void pop_back();
+    void pop_front();
+    T const & back() const;
+    T const & front() const;
+    T & back();
+    T & front();
+    struct iterator;
+    struct const_iterator;
+    typedef T value_type;
+    typedef std::ptrdiff_t difference_type;
+    typedef T* pointer;
+    typedef T& reference;
+    typedef std::bidirectional_iterator_tag iterator_category;
+    typedef std::reverse_iterator<iterator> reverse_iterator;
+    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+    iterator rbegin()
+    {
+        return iterator (this);
+    }
+    iterator rend()
+    {
+        return iterator (next);
+    }
+    const_iterator rbegin() const
+    {
+        return const_iterator (const_cast <list_base *> (this));
+    }
+    const_iterator rend() const
+    {
+        return const_iterator (const_cast <list_base *> (next));
+    }
+    iterator begin()
+    {
+        return iterator (next);
+    }
+    iterator end()
+    {
+        return iterator (this);
+    }
+    const_iterator begin() const
+    {
+        return const_iterator (const_cast <list_base *> (next));
+    }
+    const_iterator end() const
+    {
+        return const_iterator (const_cast <list_base *> (this));
+    }
+    iterator insert (const_iterator to, T const & a)
+    {
+        list_base <T> * tmp = static_cast <list_base *> (new list <T> (a));
+        const_iterator to_ = to;
+        to--;
+        merge(tmp, const_cast <list_base *> (to_.get()));
+        merge(const_cast <list_base *> (to.get()), tmp);
+        return static_cast <iterator> (tmp->next);
+    }
+    iterator erase (const_iterator to)
+    {
+        list_base * tmp = split(const_cast <list_base *> (to.get()));
+        delete to.get();
+        return static_cast <iterator> (tmp);
+    }
+    iterator erase (const_iterator a, const_iterator b)
+    {
+        if (a == b)
+            return b;
+        list_base * l = split_l(const_cast <list_base *> (a.get()));
+        list_base * r = split_r(const_cast <list_base *> (b.get()));
+        merge(l, r);
+        delete a.get();
+        return b;
+    }
+    void splice (const_iterator pos, list_base & other, const_iterator a, const_iterator b)
+    {
+        if (a == b)
+            return;
+        list_base * m = const_cast <list_base *> (b.get()->prev);
+        list_base * l = split_l(const_cast <list_base *> (a.get()));
+        list_base * r = split_r(const_cast <list_base *> (b.get()));
+        const_iterator pos_ = pos;
+        pos--;
+        merge(l, r);
+        merge(const_cast <list_base *> (pos.get()), const_cast <list_base *> (a.get()));
+        merge(m, const_cast <list_base *> (pos_.get()));
+    }
+    list_base & operator = (list_base const & c)
+    {
+        swap(c);
+        return (*this);
+    }
+};
+
+template <typename T>
+void swap(list_base <T> & a, list_base <T> & b)
+{
+    std::swap(a.next, b.next);
+    std::swap(a.prev, b.prev);
+}
+template <typename T> list_base <T>::
+list_base ():
+    prev(this),
+    next(this)
+{
+}
+template <typename T> list_base <T>::
+list_base (T const & a):
+    prev(this),
+    next(this)
+{
+}
+template <typename T> list_base <T>::
+~list_base ()
+{
+    if (empty())
+        return;
+    if (next)
+        next->prev = 0;
+    if (prev)
+        prev->next = 0;
+    if (next)
+    {
+        delete next;
+    }
+}
+template <typename T> list_base <T>::
+list_base (list_base const & a):
+        prev(this),
+        next(this)
+{
+    for (iterator i = a.begin(); i != a.end(); ++i)
+        insert(this, *i);
+}
+template <typename T> void list_base <T>::
+swap (list_base b)
+{
+    if (b.empty())
+    {
+        clear();
+        return;
+    }
+    std::swap(next, b.next);
+    std::swap(prev, b.prev);
+    if (prev)
+        prev->next = this;
+    if (next)
+        next->prev = this;
+}
+template <typename T> bool list_base <T>::
+empty ()
+{
+    return (this == next && this == prev);
+}
+template <typename T> void list_base <T>::
+clear()
+{
+    if (empty())
+        return;
+    next->prev = 0;
+    if (prev)
+        prev->next = 0;
+    delete next;
+    next = this;
+    prev = this;
+}
+template <typename T> void list_base <T>::
+push_front(T const & a)
+{
+    insert(begin(), a);
+}
+template <typename T> void list_base <T>::
+pop_front()
+{
+    erase(begin());
+}
+template <typename T> T const & list_base <T>::
+front() const
+{
+    return static_cast <list <T> *> (next)->val;
+}
+template <typename T> T & list_base <T>::
+front()
+{
+    return static_cast <list <T> *> (next)->val;
+}
+template <typename T> void list_base <T>::
+push_back(T const & a)
+{
+    insert(end(), a);
+}
+template <typename T> void list_base <T>::
+pop_back()
+{
+    erase(end());
+}
+template <typename T> T const & list_base <T>::
+back() const
+{
+    return static_cast <list <T> *> (prev)->val;
+}
+template <typename T> T & list_base <T>::
+back()
+{
+    return static_cast <list <T> *> (prev)->val;
+}
+template <typename T> struct list_base <T>::
+iterator
+{
+private:
+    list_base * val;
+public:
+    list_base * get()
+    {
+        return val;
+    }
+    iterator():
+        val(0)
+    {}
+    iterator(const_iterator a):
+        val(const_cast <list_base *> (a.get()))
+    {}
+    iterator(list_base * a):
+        val(a)
+    {}
+    iterator & operator ++ ()
+    {
+        val = val->next;
+        return *this;
+    }
+    iterator & operator -- ()
+    {
+        val = val->prev;
+        return *this;
+    }
+    iterator operator ++ (int)
+    {
+        val = val->next;
+        return val->prev;
+    }
+    iterator operator -- (int)
+    {
+        val = val->prev;
+        return val->next;
+    }
+    T & operator * ()
+    {
+        return static_cast <list <T> *> (val)->val;
+    }
+    bool operator != (iterator a)
+    {
+        return a.val != val;
+    }
+    bool operator == (iterator a)
+    {
+        return a.val == val;
+    }
+};
+template <typename T> struct list_base <T>::
+const_iterator
+{
+private:
+    list_base const * val;
+public:
+    list_base * get()
+    {
+        return const_cast <list_base *> (val);
+    }
+    const_iterator(list_base * a):
+        val(a)
+    {}
+    const_iterator():
+        val(0)
+    {}
+    const_iterator(iterator a):
+        val(a.get())
+    {}
+    const_iterator operator ++ ()
+    {
+        val = val->next;
+        return val;
+    }
+    const_iterator operator -- ()
+    {
+        val = val->prev;
+        return val;
+    }
+    const_iterator operator ++ (int)
+    {
+        val = val->next;
+        return val->prev;
+    }
+    const_iterator operator -- (int)
+    {
+        val = val->prev;
+        return val->next;
+    }
+    bool operator != (const_iterator a)
+    {
+        return a.val != val;
+    }
+    bool operator == (const_iterator a)
+    {
+        return a.val == val;
+    }
+    T const & operator * ()
+    {
+        return static_cast <list <T> *> (val)->val;
+    }
+};
+template <typename T> 
+struct list : list_base <T>
+{
+public:
+    T val;
+    list (T const & a):
+        val(a)
+    {
+    }
+};
---
+++ zachet_2/main.cpp
@@ -0,0 +1,81 @@
+#include <cstdio>
+#include "list.cpp"
+using namespace std;
+
+int main()
+{
+    cout << " ____ " << endl;
+    list_base <int> kek;
+    kek.insert(kek.begin(), 7);
+    kek.insert(kek.begin(), 8);
+    kek.insert(kek.begin(), 9);
+    for (list_base <int>::iterator i = kek.begin(); i != kek.end(); ++i)
+    {
+        cout << *i << endl;
+    }
+    cout << " ____erase " << endl;
+    kek.erase(++kek.begin());
+    cout << " ____kek " << kek.back() << endl;
+    for (list_base <int>::iterator i = kek.begin(); i != kek.end(); ++i)
+    {
+        cout << *i << endl;
+    }
+    cout << " ____kek- " << kek.front() << endl;
+    for (list_base <int>::iterator i = --kek.end(); i != kek.end(); --i)
+    {
+        cout << *i << endl;
+    }
+    list_base <int> lol;
+    cout << " ____splice " << endl;
+    lol.splice(lol.begin(), kek, kek.begin(), kek.end());
+    lol.insert(lol.end(), 7);
+    lol.insert(lol.end(), 8);
+    lol.insert(lol.end(), 9);
+    cout << " ____splice " << endl;
+    lol.splice(lol.begin(), kek, kek.begin(), kek.end());
+    cout << " ____kek " << kek.front() << endl;
+    for (list_base <int>::iterator i = kek.begin(); i != kek.end(); ++i)
+    {
+        cout << *i << endl;
+    }
+    cout << " ____kek- " << kek.back() << endl;
+    for (list_base <int>::iterator i = --kek.end(); i != kek.end(); --i)
+    {
+        cout << *i << endl;
+    }
+    cout << " ____lol " << endl;
+    for (list_base <int>::iterator i = lol.begin(); i != lol.end(); ++i)
+    {
+        cout << *i << endl;
+    }
+    list_base <int>::iterator i = lol.begin();
+    ++i;
+    list_base <int>::iterator j = lol.end();
+    cout << " ____erase " << endl;
+    lol.erase(i, j);
+    cout << " ____lol " << endl;
+    for (list_base <int>::iterator i = lol.begin(); i != lol.end(); ++i)
+    {
+        cout << *i << endl;
+    }
+    cout << " ____make_tmp_(lol) " << endl;
+    list_base <int> tmp(lol);
+    cout << " ____tmp " << endl;
+    for (list_base <int>::iterator i = tmp.begin(); i != tmp.end(); ++i)
+    {
+        cout << *i << endl;
+    }
+    cout << " ____tmp- " << endl;
+    for (list_base <int>::iterator i = --tmp.end(); i != tmp.end(); --i)
+    {
+        cout << *i << endl;
+    }
+    cout << " ____tmp=kek " << endl;
+    tmp = kek;
+    cout << " ____tmp " << endl;
+    for (list_base <int>::iterator i = tmp.begin(); i != tmp.end(); ++i)
+    {
+        cout << *i << " rr " << endl;
+    }
+    
+}
